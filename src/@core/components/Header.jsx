ac26b2d02f5d7d61f8407ca361f9a5ba231280ee
princebak <princebakenga09032017@gamil.com>
2023-07-31 13:06:25 +0100

    feat: header and landing section structure set

--- src/@core/components/Header.jsx
+++ src/@core/components/Header.jsx
@@ -1,10 +1,22 @@
-
-
-import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <img src='/images/logos/l_fondation_black.png' alt='logo' width='120px' height='80px' />
+      <nav>
+        <ul>
+          <li>Acceuil</li>
+          <li>Services</li>
+          <li>A propos de nous</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <div className='headerBtnGroup'>
+        <Link href=''>Se connecter</Link>
+        <Link href=''>S'inscrire</Link>
+      </div>
+    </header>
   )
 }
 
--- src/@core/components/LandingSection.jsx
+++ src/@core/components/LandingSection.jsx
@@ -1,10 +1,16 @@
-
-
 import React from 'react'
 
 const LandingSection = () => {
   return (
-    <div>LandingSection</div>
+    <div className='landingSection'>
+      <div className='landingSectionTextZone'>
+        <h1>Lingomba fondation est une entreprise spécialisée dans les domaines de finance</h1>
+        <h3> Participer au development communautaire est notre objectif</h3>
+      </div>
+      <div className='landingSectionImageZone'>
+        <img src='/images/lf1.jpg' alt='landingSectionImage' width='400px' height='400px' />
+      </div>
+    </div>
   )
 }
 
