4fe309a54715393ae1ca2529426a96f15d2514f9
princebak <princebakenga09032017@gamil.com>
2023-09-04 08:01:15 +0100

    fix: User and dbConnector imports

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,5 +1,5 @@
-import dbConnect from "../../../lib/dbConnect";
-import User from "../../../model/User";
+import User from 'src/models/User'
+import { dbConnector } from 'src/utils/dbConnector'
 
 export default async function handler(req, res) {
   /* switch(req.method){
@@ -7,19 +7,17 @@ export default async function handler(req, res) {
 
         }
     } */
-  console.log("API METHOD", req.method);
-  if (req.method !== "GET") {
-    return res
-      .status(400)
-      .json({ error: "This API call only accepts GET method." });
+  console.log('API METHOD', req.method)
+  if (req.method !== 'GET') {
+    return res.status(400).json({ error: 'This API call only accepts GET method.' })
   }
 
-  await dbConnect();
+  await dbConnector()
 
   try {
-    const users = await User.find();
-    res.status(200).json(users);
+    const users = await User.find()
+    res.status(200).json(users)
   } catch (error) {
-    res.status(500).json({ error: error });
+    res.status(500).json({ error: error })
   }
 }
